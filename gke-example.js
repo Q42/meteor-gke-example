4b0f3db8670daf824a5d66fa54ac7a7bfcd33cf9
Rahul Choudhury <rchoudhury@gmail.com>
2015-01-10 18:11:10 -0800

    Show which users are connected

--- gke-example.js
+++ gke-example.js
@@ -1,3 +1,5 @@
+UserConnections = new Mongo.Collection("user_status_sessions");
+
 if (Meteor.isClient) {
 
   Meteor.startup(function() {
@@ -5,20 +7,26 @@ if (Meteor.isClient) {
     Session.setDefault('ip', null);
   });
 
+  Template.address.events({
+    'click #body': function() {
+    }
+  })
+
   Template.address.helpers({
-    ips: function() {
-      Meteor.call("getIps", function(err, res) {
-        if (res)
-          Session.set("ips", res);
-      });
-      return Session.get("ips");
+    user: function() {
+      return UserConnections.find({userId: {$exists: false}});
+    },
+    x: function() {
+      return ~~(Math.random() * $(window).width());
+    },
+    y: function() {
+      return ~~(Math.random() * $(window).height());
     },
     color: function() {
-      //var ip = Session.get("ips")[0].address;
       var ip = Session.get('ip');
       if (ip) {
         var color = "rgba(" + _.tail(ip.split(".")).join(",") + ", .9)";
-        var color2 = "rgba(" + _.map(_.tail(ip.split(".")), function(c) { return c - 25}).join(",") + ", .7)";
+        var color2 = "rgba(" + _.map(_.tail(ip.split(".")), function(c) { return c - 25}).join(",") + ", .9)";
 
         var style = "radial-gradient(closest-corner,rgba(16,47,70,0) 60%,rgba(16,47,70,0.26)),linear-gradient(108deg," + color + "," + color2 + " 90%);";
         return style;
@@ -33,6 +41,16 @@ if (Meteor.isClient) {
     }
   })
 
+  Tracker.autorun(function(c) {
+    try {
+      UserStatus.startMonitor({
+        threshold: 30000,
+        idleOnBlur: true
+      });
+      c.stop();
+    } catch(e) {}
+  });
+
 }
 
 if (Meteor.isServer) {
@@ -41,29 +59,11 @@ if (Meteor.isServer) {
   var ifaces = os.networkInterfaces();
   var ip = null;
 
+  Meteor.publish(null, function() {
+    return UserStatus.connections.find();
+  });
+
   Meteor.methods({
-
-    getIps: function() {
-      // http://stackoverflow.com/a/8440736/16308
-      var result = [];
-      Object.keys(ifaces).forEach(function (ifname) {
-        ifaces[ifname].forEach(function (iface) {
-          if ('IPv4' !== iface.family || iface.internal !== false) {
-            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-            return;
-          }
-
-          result.push({
-            ifname: ifname,
-            address: iface.address
-          });
-          //console.log(result);
-          
-        });
-      });
-
-      return result;
-    },
     getIp: function() {
       if (ip) return ip;
       var result = HTTP.get('http://api.ipify.org', {timeout:5000});
@@ -76,6 +76,5 @@ if (Meteor.isServer) {
         throw new Meteor.Error(result.statusCode, errorJson.error);
       }
     }
-
   });
 }
